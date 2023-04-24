aaee46529e6a5f326dfddcfe003ad484bd218a44
prerna bhardwaj <109895343+prernabharadwaj-Code@users.noreply.github.com>
2023-04-24 22:29:39 +0530

    Create objectbymethod.java

---
+++ objectbymethod.java
@@ -0,0 +1,23 @@
+package prernabhardwaj;
+
+public class overloading3 {
+	void number (int a,int b)
+	{
+		System.out.println(a*b);
+	}
+	void number1 (double a,double b) {
+		System.out.println(a*b);
+		
+	}
+
+	 
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub\
+		overloading3 obj=new overloading3();
+		obj.number(2,3);
+		
+
+	}
+
+}
