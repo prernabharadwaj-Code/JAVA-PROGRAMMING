9cb70d3773d8b8ace1f097745ef8cf7e762505d7
prerna bhardwaj <109895343+prernabharadwaj-Code@users.noreply.github.com>
2023-05-01 10:14:22 +0530

    Create program 3 .java

---
+++ program 3 .java
@@ -0,0 +1,17 @@
+package prernabhardwaj;
+
+public class Simpleobject {
+	Simpleobject(){
+		System.out.println("hello everyone ,hoew are u all ");
+	}
+}
+class Doubleobject{ 
+	public static void main(String[] args) {
+		Simpleobject simple = new Simpleobject();
+		
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
